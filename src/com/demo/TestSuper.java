e735998287bd635d2799ef9163c60a12e0080383
wangzhao <wangzhao@go-goal.com>
2019-01-08 19:41:58 +0800

    super

---
+++ src/com/demo/TestSuper.java
@@ -0,0 +1,29 @@
+package com.demo;
+
+class Animal {
+	String color = "black";
+
+	void eating() {
+		System.out.println("eating...");
+	}
+}
+
+public class TestSuper extends Animal {
+	String color = "white";
+
+	void eating() {
+		System.out.println("eating again....");
+	}
+
+	void display() {
+		eating();
+		super.eating();
+		System.out.println(color);
+		System.out.println(super.color);
+	}
+
+	public static void main(String[] args) {
+		TestSuper aSuper = new TestSuper();
+		aSuper.display();
+	}
+}
